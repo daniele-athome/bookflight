fd4581424df114077f202fce01054b812fa2bf0f
Daniele Ricci <daniele@casaricci.it>
2020-09-19 00:22:37 +0200

    Move database to its own directory
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- server/config.php
+++ server/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'database' => 'sqlite:' . BASE_DIR . '/bookflight.db',
+    'database' => 'sqlite:' . BASE_DIR . '/data/bookflight.db',
     'google_calendar_credentials' => BASE_DIR . DIRECTORY_SEPARATOR . 'service_account.json',
     'google_calendar_id' => '9kf3jm0tpcfvpc6i8qe7fhm1p0@group.calendar.google.com',
 ];
