7bb978a6d1e18d868279071722595f2b594f6302
Daniele Ricci <daniele@casaricci.it>
2020-11-13 10:11:13 +0100

    Test events
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

---
+++ test_events.js
@@ -0,0 +1,33 @@
+{
+  events: [
+    {
+      title: 'Paolo',
+      start: '2020-11-13T16:00:00',
+      end: '2020-11-13T18:00:00',
+      extendedProps: {
+        description: 'Esami',
+      }
+    },
+    {
+      title: 'Daniele',
+      start: '2020-11-14T16:00:00',
+      end: '2020-11-14T18:00:00',
+      extendedProps: {
+        description: 'Istruzionale',
+      }
+    },
+    {
+      title: 'Manuel',
+      start: '2020-11-14T18:00:00',
+      end: '2020-11-14T23:00:00',
+      extendedProps: {
+        description: 'Volo notturno con testo lungo descrizione lunghissima non sapevo cosa scrivere',
+      }
+    },
+    {
+      title: 'Claudia',
+      start: '2020-11-15T09:00:00',
+      end: '2020-11-15T10:00:00',
+    },
+  ]
+}
