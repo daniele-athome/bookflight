21bf3ebcd53a495fa50fe799d35f4ef85d3c9825
Daniele Ricci <daniele@casaricci.it>
2020-11-23 20:19:50 +0100

    Remove prod configuration
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- ionic/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    googleCalendarApiKey: 'AIzaSyDijFzSBlkyl_zVH2gQuaokJpm7y-mIKkg',
-    events: '9kf3jm0tpcfvpc6i8qe7fhm1p0@group.calendar.google.com',
-};
